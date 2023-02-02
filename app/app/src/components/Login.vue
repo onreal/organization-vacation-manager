244e58d91a42e6e03c56438243c2d4f714055383
Margarit Koka <panagiotis.kokas@gmail.com>
2023-02-02 16:16:15 +0200

    feat: initial vuejs app

---
+++ app/app/src/components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <form @submit.prevent="login">
+      <h2 class="mb-3">Login</h2>
+      <div class="input">
+        <label for="email">Email address</label>
+        <input
+            class="form-control"
+            type="text"
+            name="email"
+            placeholder="email@adress.com"
+        />
+      </div>
+      <div class="input">
+        <label for="password">Password</label>
+        <input
+            class="form-control"
+            type="password"
+            name="password"
+            placeholder="password123"
+        />
+      </div>
+      <div class="alternative-option mt-4">
+      </div>
+      <button type="submit" class="mt-4 btn-pers" id="login_button">
+        Login
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login(submitEvent) {
+      this.email = submitEvent.target.elements.email.value;
+      this.password = submitEvent.target.elements.password.value;
+    },
+    moveToRegister() {
+    },
+  },
+};
+</script>
