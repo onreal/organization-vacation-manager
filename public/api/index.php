b8c38863d44952a9d18dff7b8b451dc7f7a384ad
Margarit Koka <panagiotis.kokas@gmail.com>
2023-02-09 21:58:28 +0200

    chore: set api cors

--- public/api/index.php
+++ public/api/index.php
@@ -1,12 +1,19 @@
 <?php
 
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Headers:X-Request-With');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
 use DI\Bridge\Slim\Bridge;
 use DI\ContainerBuilder;
+use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Psr7\Request;
 use OpenApi\Annotations\Get;
 use OpenApi\Annotations\Response;
 use OpenApi\Annotations\Info;
 use OpenApi\Annotations\PathItem;
+use Slim\Routing\RouteContext;
 
 include '../../app/api/vendor/autoload.php';
 
@@ -20,33 +27,17 @@ try {
     $container = $containerBuilder->build();
     // Attach dependencies on Slim
     $app = Bridge::create($container);
-    // Register Slim routes
     require '../../app/api/src/Api/Routes/user.php';
     require '../../app/api/src/Api/Routes/application.php';
     require '../../app/api/src/Api/Routes/logAction.php';
-    // require '../app/src/Core/Domain/Entities/User.php';
-    // Register Swagger route
-    /**
-     * @Get(
-     *     path="/openapi",
-     *     tags={"documentation"},
-     *     summary="OpenAPI JSON File that describes the API",
-     *     @OA\Response(response="200", description="OpenAPI Description File"),
-     * )
-     */
-    $app->get('/openapi', function ($request, $response) {
-        $swagger = \OpenApi\Generator::scan(['../../app/api/src/Api/Routes']);
-        $response->getBody()->write(json_encode($swagger));
-        return $response->withHeader('Content-Type', 'application/json');
-    });
-    // Add slim middleware for all requests to json
-    $applicationJsonMiddleware = function (Request $request, Slim\Routing\RouteRunner $handler) {
+    // Add slim CORS
+    $app->add(function ($request, $handler) {
+        // Set content type
         $request = $request->withHeader('Content-Type', 'application/json');
         return $handler->handle($request);
-    };
-    $app->add($applicationJsonMiddleware);
+    });
     // Run Slim app
     $app->run();
-} catch (Exception $e) {
+} catch (Exception|Throwable $e) {
     die('Something wrong happened during app initialization. Message: '. $e->getMessage());
 }
